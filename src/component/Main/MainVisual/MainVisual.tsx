ecddc9f703317d4860c543bcff2a3fffaa483edb
이휘경 <ter0000@naver.com>
2024-01-25 19:48:13 +0900

    Tab_MainVisual

--- src/component/Main/MainVisual/MainVisual.tsx
+++ src/component/Main/MainVisual/MainVisual.tsx
@@ -10,6 +10,11 @@ function MainVisual() {
             src={`${process.env.PUBLIC_URL}/assets/newyear_logo.png`}
             alt="Peak Holiday VIBES"
           />
+          <img
+            className="tab-slogan"
+            src={`${process.env.PUBLIC_URL}/assets/tab_newyear_logo.png`}
+            alt="Peak Holiday VIBES"
+          />
         </div>
         <div className="btn_main_visual_slogan">
           <a href="./">자세히보기</a>
@@ -21,6 +26,11 @@ function MainVisual() {
               src={`${process.env.PUBLIC_URL}/assets/pcDrink1.png`}
               alt="푸른 용 클래식 밀크티 이미지"
             />
+            <img
+              className="tab-drink"
+              src={`${process.env.PUBLIC_URL}/assets/tab_drink1.png`}
+              alt="푸른 용 클래식 밀크티 이미지"
+            />
           </div>
           <div className="set_common set_02">
             <img
@@ -28,11 +38,21 @@ function MainVisual() {
               src={`${process.env.PUBLIC_URL}/assets/pcDrink2.png`}
               alt="푸른 용 헤이즐넛 라떼 이미지"
             />
+            <img
+              className="tab-drink"
+              src={`${process.env.PUBLIC_URL}/assets/tab_drink2.png`}
+              alt="푸른 용 헤이즐넛 라떼 이미지"
+            />
           </div>
           <div className="set_common set_03">
             <img
               className="pc-drink"
               src={`${process.env.PUBLIC_URL}/assets/pcDrink3.png`}
+              alt="핑크 폼 딸기 라떼 이미지"
+            />
+            <img
+              className="tab-drink"
+              src={`${process.env.PUBLIC_URL}/assets/tab_drink3.png`}
               alt="핑크 폼 딸기 라떼 이미지"
             />
           </div>
