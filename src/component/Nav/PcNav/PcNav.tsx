d8b33a6081eb796eecb5a77d3835c49dfb6aaa69
이휘경 <ter0000@naver.com>
2024-02-13 12:41:39 +0900

    pc nav 완

--- src/component/Nav/PcNav/PcNav.tsx
+++ src/component/Nav/PcNav/PcNav.tsx
@@ -170,44 +170,327 @@ function PcNav() {
                   </li>
                 </ul>
                 <ul className="dropDown_content_item">
-                  <li className="dropDown_content_title">스타벅스 리저브</li>
-                  <li>RESERVE MAGAZINE</li>
-                </ul>
-                <ul className="dropDown_content_item">
-                  <li className="dropDown_content_title">에스프레소 음료</li>
-                  <li>도피오</li>
-                  <li>에스프레소 마키아또</li>
-                  <li>아메리카노</li>
-                  <li>마키아또</li>
-                  <li>카푸치노</li>
-                  <li>라떼</li>
-                  <li>모카</li>
+                  <li className="dropDown_content_title">
+                    <a href=" ">상품</a>
+                  </li>
+                  <li>
+                    <a href=" ">머그</a>
+                  </li>
+                  <li>
+                    <a href=" ">글라스</a>
+                  </li>
+                  <li>
+                    <a href=" ">플라스틱 텀블러</a>
+                  </li>
+                  <li>
+                    <a href=" ">보온병</a>
+                  </li>
+                  <li>
+                    <a href=" ">액세서리</a>
+                  </li>
+                  <li>
+                    <a href=" ">선물세트</a>
+                  </li>
+                  <li>
+                    <a href=" ">커피 용품</a>
+                  </li>
+                  <li>
+                    <a href=" ">패키지 티(티바나)</a>
+                  </li>
+                  <li>
+                    <a href=" ">시럽</a>
+                  </li>
                 </ul>
                 <ul className="dropDown_content_item">
                   <li className="dropDown_content_title">
-                    최상의 커피를 즐기는 법
+                    <a href=" ">카드</a>
                   </li>
-                  <li>커피 프레스</li>
-                  <li>푸어 오버</li>
-                  <li>아이스 푸어 오버</li>
-                  <li>커피 메이커</li>
+                  <li>
+                    <a href=" ">실물카드</a>
+                  </li>
+                  <li>
+                    <a href=" ">e-Gift카드</a>
+                  </li>
+                </ul>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">메뉴 이야기</a>
+                  </li>
+                  <li>
+                    <a href=" ">스타벅스 티바나</a>
+                  </li>
+                  <li>
+                    <a href=" ">스타벅스 콜드 브루</a>
+                  </li>
                 </ul>
               </DropDownContent>
             </li>
             <li className="sub_gnb_nav_item3">
               <h2>STORE</h2>
+              <DropDownContent>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">매장 찾기</a>
+                  </li>
+                  <li>
+                    <a href=" ">퀵 검색</a>
+                  </li>
+                  <li>
+                    <a href=" ">지역 검색</a>
+                  </li>
+                </ul>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">드라이브 스루 매장</a>
+                  </li>
+                </ul>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">스타벅스 리저브 매장</a>
+                  </li>
+                </ul>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">커뮤니티 스토어 매장</a>
+                  </li>
+                </ul>
+              </DropDownContent>
             </li>
             <li className="sub_gnb_nav_item4">
               <h2>RESPONSIBILITY</h2>
+              <DropDownContent>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">사회공헌 캠페인 소개</a>
+                  </li>
+                </ul>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">지역 사회 참여 활동</a>
+                  </li>
+                  <li>
+                    <a href=" ">희망배달 캠페인</a>
+                  </li>
+                  <li>
+                    <a href=" ">재능기부 카페 소식</a>
+                  </li>
+                  <li>
+                    <a href=" ">커뮤니티 스토어</a>
+                  </li>
+                  <li>
+                    <a href=" ">청년 지원 프로그램</a>
+                  </li>
+                  <li>
+                    <a href=" ">우리 농산물 사랑 캠페인</a>
+                  </li>
+                  <li>
+                    <a href=" ">우리 문화 지키기</a>
+                  </li>
+                </ul>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">환경보호 활동</a>
+                  </li>
+                  <li>
+                    <a href=" ">친환경 활동</a>
+                  </li>
+                  <li>
+                    <a href=" ">일회용 컵 없는 매장</a>
+                  </li>
+                  <li>
+                    <a href=" ">커피 원두 재활용</a>
+                  </li>
+                </ul>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">윤리 구매</a>
+                  </li>
+                  <li>
+                    <a href=" ">윤리적 원두 구매</a>
+                  </li>
+                  <li>
+                    <a href=" ">공정무역 인증</a>
+                  </li>
+                  <li>
+                    <a href=" ">커피 농가 지원 활동</a>
+                  </li>
+                </ul>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">글로벌 사회 공헌</a>
+                  </li>
+                  <li>
+                    <a href=" ">윤리경영 보고서</a>
+                  </li>
+                  <li>
+                    <a href=" ">스타벅스 재단</a>
+                  </li>
+                  <li>
+                    <a href=" ">지구촌 봉사의 달</a>
+                  </li>
+                </ul>
+              </DropDownContent>
             </li>
             <li className="sub_gnb_nav_item5">
               <h2>STARBUCKS REWARDS</h2>
+              <DropDownContent>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">스타벅스 리워드</a>
+                  </li>
+                  <li>
+                    <a href=" ">스타벅스 리워드 소개</a>
+                  </li>
+                  <li>
+                    <a href=" ">등급 및 혜택</a>
+                  </li>
+                  <li>
+                    <a href=" ">스타벅스 별</a>
+                  </li>
+                  <li>
+                    <a href=" ">자주하는 질문</a>
+                  </li>
+                </ul>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">스타벅스 카드</a>
+                  </li>
+                  <li>
+                    <a href=" ">스타벅스 카드 소개</a>
+                  </li>
+                  <li>
+                    <a href=" ">스타벅스 카드 갤러리</a>
+                  </li>
+                  <li>
+                    <a href=" ">등록 및 조회</a>
+                  </li>
+                  <li>
+                    <a href=" ">충전 및 이용안내</a>
+                  </li>
+                  <li>
+                    <a href=" ">분실신고/환불신청</a>
+                  </li>
+                  <li>
+                    <a href=" ">자주 하는 질문</a>
+                  </li>
+                </ul>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">스타벅스 e-Gift Card</a>
+                  </li>
+                  <li>
+                    <a href=" ">스타벅스 e-Gift Card 소개</a>
+                  </li>
+                  <li>
+                    <a href=" ">이용안내</a>
+                  </li>
+                  <li>
+                    <a href=" ">선물하기</a>
+                  </li>
+                  <li>
+                    <a href=" ">자주 하는 질문</a>
+                  </li>
+                </ul>
+              </DropDownContent>
             </li>
             <li className="sub_gnb_nav_item6">
               <h2>CORPORATE SALES</h2>
+              <DropDownContent>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">단체 및 기업 구매 안내</a>
+                  </li>
+                  <li>
+                    <a href=" ">MD 상품</a>
+                  </li>
+                  <li>
+                    <a href=" ">실물카드</a>
+                  </li>
+                  <li>
+                    <a href=" ">e-Gift Card(MMS)</a>
+                  </li>
+                  <li>
+                    <a href=" ">e-Gift Card(BULK)</a>
+                  </li>
+                </ul>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">단체 주문 배달 안내</a>
+                  </li>
+                  <li>
+                    <a href=" ">단체 주문 배달 안내</a>
+                  </li>
+                  <li>
+                    <a href=" ">단체 주문 신청</a>
+                  </li>
+                  <li>
+                    <a href=" ">신청 내역 조회</a>
+                  </li>
+                </ul>
+              </DropDownContent>
             </li>
             <li className="sub_gnb_nav_item7">
               <h2>NEW</h2>
+              <DropDownContent>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">이벤트</a>
+                  </li>
+                  <li>
+                    <a href=" ">전체</a>
+                  </li>
+                  <li>
+                    <a href=" ">스타벅스 카드</a>
+                  </li>
+                  <li>
+                    <a href=" ">스타벅스 리워드</a>
+                  </li>
+                  <li>
+                    <a href=" ">온라인</a>
+                  </li>
+                </ul>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">뉴스</a>
+                  </li>
+                  <li>
+                    <a href=" ">전체</a>
+                  </li>
+                  <li>
+                    <a href=" ">상품 출시</a>
+                  </li>
+                  <li>
+                    <a href=" ">스타벅스와 문화</a>
+                  </li>
+                  <li>
+                    <a href=" ">스타벅스 사회공헌</a>
+                  </li>
+                  <li>
+                    <a href=" ">스타벅스 카드출시</a>
+                  </li>
+                </ul>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">매장별 이벤트</a>
+                  </li>
+                  <li>
+                    <a href=" ">일반 매장</a>
+                  </li>
+                  <li>
+                    <a href=" ">신규 매장</a>
+                  </li>
+                </ul>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">공지사항</a>
+                  </li>
+                </ul>
+                <ul className="dropDown_content_item">
+                  <li className="dropDown_content_title">
+                    <a href=" ">월페이퍼</a>
+                  </li>
+                </ul>
+              </DropDownContent>
             </li>
           </ul>
         </div>
